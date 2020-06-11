cc72052ef39888f957cde42b2b3c9ee57d5dba12
wangjunjun <trunin@163.com>
2020-06-11 23:33:46 +0800

    头部信息学习

--- src/main.js
+++ src/main.js
@@ -4,6 +4,13 @@ const app = express();
 
 const port = 3000;
 
+/**
+ *  使用JSON格式的插件
+ */
+
+app.use( express.json());
+
+
 app.listen(port, () => {
 
     console.log('服务已启动')
@@ -41,4 +48,21 @@ app.get('/posts/:postId', (req, res) => {
 
     const posts = data.filter(item => item.id == postId);
     res.send(posts[0]);
-})
+});
+
+app.post('/posts', (req, res) => {
+    const {content} = req.body;
+
+    //设置响应状态码
+    res.status(201);
+
+    // 输出请求头部数据
+    console.log(req.headers['sing-along']);
+
+    // 设置头部数据
+    res.set('Sing-Along', 'How I wonder what you are!')
+
+    res.send({
+        message: `成功创建了内容：${content}`
+    });
+});
